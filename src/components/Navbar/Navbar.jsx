0864294c1d0f6e3f2dd7586a8e58b584de14ae0c
gercamjr <gercamjr.dev@gmail.com>
2022-05-16 13:35:26 -0500

    added images

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import './Navbar.scss'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav>
+        <img src={images.logo} alt="Logo" />
+        </nav>
+    )
 }
 
 export default Navbar
