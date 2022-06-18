4d0b126047ff523355998a8c239b38701bd6f5dd
gercamjr <gercamjr.dev@gmail.com>
2022-06-18 16:11:31 -0500

    added ganalytics tracker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import ReactGa from 'react-ga'
+const TRACKING_ID = "G-J10WY67PXL"
+ReactGa.initialize(TRACKING_ID)
+
 import React from "react";
+
+import RouteChangeTracker from './components/RouteChangeTracker';
 
 import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
 import { Navbar } from "./components";
@@ -7,6 +13,8 @@ import './App.scss'
 
 const App = () => {
   return (
+    <>
+    <RouteChangeTracker />
     <div className="app">
       <Navbar />
       <Header />
@@ -16,6 +24,7 @@ const App = () => {
       <Testimonials />
       <Footer />
     </div>
+    </>
   );
 };
 
